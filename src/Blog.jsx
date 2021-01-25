617b74aad6d7127104dad625232073bf49f84507
hirotakaito <hirotakaito@keio.jp>
2021-01-25 14:32:26 +0900

    [add] reactのチュートリアルを行った

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -1,18 +1,56 @@
 import React from "react";
 import Article from "./Article";
+import * as FooBar from "./components/FooBar";
+import Hoge from "./components/Hoge";
 
 class Blog extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      isPublished: true,
+      count: 0,
+    };
   }
+
+  componentDidMount(){
+    //ボタンがクリックされたらいいねを増やす
+    document.getElementById("counter").addEventListener("click",()=>this.countUp())
+  }
+
+  componentDidUpdate(){
+    if(this.state.count >= 10){
+      this.setState({count:0})
+    }
+  }
+  componentWillUnmount(){
+    document.getElementById("counter").removeEventListener("click",()=>this.countUp())
+  }
+
+  //公開状態を変更する関数
+  togglePublished = () => {
+    this.setState({
+      isPublished: !this.state.isPublished,
+    });
+  };
+  //いいね数を増やす関数
+  countUp = () => {
+    this.setState({count:this.state.count+1})
+  }
+
   render() {
-  
     return (
       <>
-        <Article title = {"reactの使い方"}/>
-        <Article title = {"jsxの使い方"}/>
-        <Article title = {"環境構築をしてみよう"}/>
-       
+        {/* <React.Fragment></React.Fragment>で囲ってもOK */}
+        {/* 関数を渡す時に、props名= {somefunc()}としてしまうと関数が評価される時にstateの値が変化し、再度renderがおこり、無限ループになってしまうのでpropsName={()=>somefunc()}と渡し方を工夫する */}
+        <Article
+          title={"reactの使い方"}
+          isPublished={this.state.isPublished}
+          toggle={() => this.togglePublished()}
+          count={this.state.count}
+        />
+        <FooBar.Foo />
+        <FooBar.Bar />
+        <Hoge />
       </>
     );
   }
---
+++ src/components/FooBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+export function Foo(){
+    return(<h2>Fooooooooooo</h2>)
+};
+
+export const Bar = () => {
+    return (<h2>Barrrrrrrrrrr</h2>)
+};
